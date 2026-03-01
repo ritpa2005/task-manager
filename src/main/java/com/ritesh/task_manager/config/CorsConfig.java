6c4790bc50bd28a91bedf0f9f42d04f2f8718de7
Ritesh Kumar Panda <ritesh.ku.panda.925@gmail.com>
2026-03-01 20:59:31 +0530

    Update allowed origins in CORS configuration

--- src/main/java/com/ritesh/task_manager/config/CorsConfig.java
+++ src/main/java/com/ritesh/task_manager/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://your-frontend.com")
+                        .allowedOrigins("https://task-manager-fex9.onrender.com")
                         .allowedMethods("*");
             }
         };
